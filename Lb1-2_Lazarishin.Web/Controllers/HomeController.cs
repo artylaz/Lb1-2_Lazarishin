df566f9930d91a446cffc1b510dd33a90e2c0439
Arty <arty-laz@mail.ru>
2022-12-29 05:50:11 +0500

    Реализовал нормализацию данных
    - Добавил минимальное и максимальное значение для каждой строки

--- Lb1-2_Lazarishin.Web/Controllers/HomeController.cs
+++ Lb1-2_Lazarishin.Web/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Lb1_2_Lazarishin.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
 
 namespace Lb1_2_Lazarishin.Web.Controllers
 {
@@ -28,6 +30,72 @@ namespace Lb1_2_Lazarishin.Web.Controllers
             else
                 return View();
         }
+
+        [HttpGet]
+        public IActionResult Normalization(IndexVM indexVM)
+        {
+            //Преобразуем лист<string> в лист<double>
+            indexVM = GetValuesDoouble(indexVM);
+            if (indexVM == null)
+                return View();
+
+            //Находим минимальные и максимальные значения для каждой строки
+            //var (minValues, maxValues) = GetMinMaxValues(indexVM);
+
+            //Нормализация
+
+            for (int i = 0; i < indexVM.Rows.Count; i++)
+            {
+                for (int j = 0; j < indexVM.Questionnaires.Count; j++)
+                {
+                    var result = (indexVM.Questionnaires[j].Values[i] - indexVM.Rows[i].MinValueRow) / (indexVM.Rows[i].MaxValueRow - indexVM.Rows[i].MinValueRow);
+
+                    if (double.IsNaN(result))
+                        indexVM.Questionnaires[j].Values[i] = 0;
+                    else
+                    {
+                        if (result % 1 == 0)
+                            indexVM.Questionnaires[j].Values[i] = result;
+                        else
+                            indexVM.Questionnaires[j].Values[i] = Math.Round(result, 2);
+                    }
+                }
+            }
+
+            // Говрим о том, что мы нормализировали данные
+            indexVM.Status = Status.Normalization;
+
+            return View(indexVM);
+        }
+
+        /// <summary>
+        /// Преобразует значения Questionnaires из string в double и заполнятет IndexVM.Questionnaires новыми значениями
+        /// </summary>
+        /// <param name="indexVM"></param>
+        /// <returns></returns>
+        public IndexVM GetValuesDoouble(IndexVM indexVM)
+        {
+            for (int i = 0; i < indexVM.Rows.Count; i++)
+            {
+                for (int j = 0; j < indexVM.Questionnaires.Count; j++)
+                {
+                    if (indexVM.Questionnaires[j].ValuesToString[i].Replace(" ", "") == "да")
+                        indexVM.Questionnaires[j].ValuesToString[i] = "1";
+                    else if (indexVM.Questionnaires[j].ValuesToString[i].Replace(" ", "") == "нет")
+                        indexVM.Questionnaires[j].ValuesToString[i] = "0";
+
+                    var valueStr = indexVM.Questionnaires[j].ValuesToString[i];
+
+                    if (double.TryParse(valueStr, out double value))
+                        indexVM.Questionnaires[j].Values.Add(value);
+                }
+            }
+
+            return indexVM;
+        }
+
+        
+
         /// <summary>
         /// </summary>
         /// <param name="indexVM"></param>
@@ -65,6 +133,37 @@ namespace Lb1_2_Lazarishin.Web.Controllers
             }
             return indexVM;
         }
+
+        ///// <summary>
+        ///// Находить для каждого ряда минимальное и максимальное значение
+        ///// </summary>
+        ///// <param name="questionnaires"></param>
+        ///// <returns>Возвращает кортеж с листом максимальных и минимальных значений для каждого ряда значений</returns>
+        //public (List<double> minValues, List<double> maxValues) GetMinMaxValues(IndexVM indexVM)
+        //{
+        //    var minValues = new List<double>();
+        //    var maxValues = new List<double>();
+
+        //    for (int i = 0; i < indexVM.Rows.Count; i++)
+        //    {
+        //        double min = -1; // Для присваивания самого первого числа
+        //        double max = 0;
+        //        for (int j = 0; j < indexVM.Questionnaires.Count; j++)
+        //        {
+        //            if (min == -1)
+        //                min = indexVM.Questionnaires[j].Values[i];
+        //            else if (indexVM.Questionnaires[j].Values[i] < min)
+        //                min = indexVM.Questionnaires[j].Values[i];
+
+        //            if (indexVM.Questionnaires[j].Values[i] > max)
+        //                max = indexVM.Questionnaires[j].Values[i];
+        //        }
+        //        minValues.Add(min);
+        //        maxValues.Add(max);
+        //    }
+
+        //    return (minValues, maxValues);
+        //}
 
     }
 }
---
+++ Lb1-2_Lazarishin.Web/Models/Row.cs
@@ -0,0 +1,9 @@
+﻿namespace Lb1_2_Lazarishin.Web.Models
+{
+    public class Row
+    {
+        public string Question { get; set; }
+        public double MinValueRow { get; set; }
+        public double MaxValueRow { get; set; }
+    }
+}
